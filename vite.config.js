babc6e19d26ec190562b6ed8a1fe6d98bc1ff136
rdpnr_wangguodong <you@example.com>
2024-01-19 13:26:32 +0800

    修改vite配置判断当前环境

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,27 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import postcssPlugins from './postcss.config.js'
 import { UnifiedViteWeappTailwindcssPlugin as uvwt } from 'weapp-tailwindcss/vite';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    uni(),
-    uvwt()
-  ],
-  css: {
-    postcss: {
-      plugins: postcssPlugins
-    }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  const serverIp = command === 'dev' ? 'http://localhost:3000' : 'https://nest-cloud-be.vercel.app';
+  return {
+    plugins: [
+      uni(),
+      uvwt()
+    ],
+    css: {
+      postcss: {
+        plugins: postcssPlugins
+      }
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: serverIp,
+          changeOrigin: true
+        },
       },
     },
-  },
+  }
 })
